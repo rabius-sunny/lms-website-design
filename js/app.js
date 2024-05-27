9f441e1485582eff994a484af40bc1d4795b3fd2
rabius-sunny <rabiussunnyc3x@gmail.com>
2024-05-28 03:14:09 +0600

    add: course-details

--- js/app.js
+++ js/app.js
@@ -29,3 +29,27 @@ const swiper = new Swiper('.swiper', {
 
 reuseComponent('../components/header.html', 'navbar-placeholder')
 reuseComponent('../components/footer.html', 'footer-placeholder')
+
+document.getElementById('show-more')?.addEventListener('click', function () {
+  var textContainer = document.getElementById('text-container')
+
+  if (textContainer.classList.contains('expanded')) {
+    textContainer.classList.remove('expanded')
+    this.innerHTML = `<div
+    class="show-more d-flex align-items-center gap-2"
+    id="show-more"
+  >
+    Show More
+    <i class="bi bi-chevron-down"></i>
+  </div>`
+  } else {
+    textContainer.classList.add('expanded')
+    this.innerHTML = `<div
+    class="show-more d-flex align-items-center gap-2"
+    id="show-more"
+  >
+    Show More
+    <i class="bi bi-chevron-up"></i>
+  </div>`
+  }
+})
